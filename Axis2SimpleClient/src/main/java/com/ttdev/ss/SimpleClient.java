81cc7244b40efcc12de659ffc3f22b653dfa6d49
Shrikant Vashishtha <vashishtha.sk@gmail.com>
2012-12-31 11:05:45 +0800

    added axis2 implementation

---
+++ Axis2SimpleClient/src/main/java/com/ttdev/ss/SimpleClient.java
@@ -0,0 +1,18 @@
+package com.ttdev.ss;
+
+import java.rmi.RemoteException;
+
+import com.ttdev.ss.SimpleServiceStub.ConcatRequest;
+import com.ttdev.ss.SimpleServiceStub.ConcatResponse;
+
+public class SimpleClient {
+	public static void main(String[] args) throws RemoteException {
+		SimpleServiceStub service = new SimpleServiceStub(
+				"http://localhost:8080/axis2/services/SimpleService");
+		ConcatRequest request = new ConcatRequest();
+		request.setS1("abc");
+		request.setS2("123");
+		ConcatResponse response = service.concat(request);
+		System.out.println(response.getConcatResponse());
+	}
+}
